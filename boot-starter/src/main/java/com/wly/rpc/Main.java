fa1eff4e14291715c693df6140322405b5481205
mewbugger <wangleyan0735@163.com>
2024-12-26 00:27:18 +0800

    init boot-starter

---
+++ boot-starter/src/main/java/com/wly/rpc/Main.java
@@ -0,0 +1,12 @@
+package com.wly.rpc;
+
+/**
+ * @author BarryCrude
+ * @date 2024-12-26 0:26
+ * @description:
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
